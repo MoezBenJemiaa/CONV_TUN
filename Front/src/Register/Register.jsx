925cc083a99369d437421e68b5ce1956b22ba99a
Moez <moez.benjemiaa@etudiant-fst.utm.tn>
2025-04-05 03:19:57 +0200

    Register update

--- Front/src/Register/Register.jsx
+++ Front/src/Register/Register.jsx
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
-import "./Register.css";
+import React, { useState, useEffect } from "react";
+import styles from "./Register.module.css";
+import {
+  FaUserCircle,
+  FaFacebook,
+  FaInstagram,
+  FaWhatsapp,
+} from "react-icons/fa";
 
-function RegisterPage() {
+export default function RegisterPage() {
   const [profilePic, setProfilePic] = useState(null);
   const [idPhoto, setIdPhoto] = useState(null);
   const [idPhotoName, setIdPhotoName] = useState(""); // State to store ID photo name
+  const [showModal, setShowModal] = useState(false);
+  const [selectedPlatform, setSelectedPlatform] = useState("");
+
+  const handleIconClick = (platform) => {
+    setSelectedPlatform(platform);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setSelectedPlatform("");
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -58,46 +76,170 @@ function RegisterPage() {
   };
 
   return (
-    <div className="Register_container">
-      <div className="Register_card">
-        <h2 className="Register_title">Inscrivez-vous</h2>
-        <div className="contentWrapper">
-          <div className="profileSection">
-            <div className="profilePic">
-              {profilePic ? (
-                <img src={URL.createObjectURL(profilePic)} alt="Profile" className="img" />
-              ) : (
-                <div className="placeholder">+</div>
-              )}
-            </div>
-            <div className="photoActions">
-              <input type="file" accept="image/*" onChange={handleImageUpload} className="hidden" id="file-upload" />
-              <label htmlFor="file-upload" className="uploadBtn">Télécharger une photo</label>
-              <button className="deleteBtn" onClick={handleDeletePhoto}>Supprimer la photo</button>
-            </div>
+    <div className={styles.registerContainer}>
+      <div className={styles.signupContainer}>
+        <div className={styles.profileSection}>
+          <label className={styles.pdp}>Photo de profil</label>
+          <div className={styles.profilePic}>
+            {profilePic ? (
+              <img
+                src={URL.createObjectURL(profilePic)}
+                alt="Profile"
+                className={styles.img}
+              />
+            ) : (
+              <FaUserCircle className={styles.icon} />
+            )}
           </div>
-          <form className="Register_form" onSubmit={handleSubmit}>
-            <input type="text" name="lastName" placeholder="Nom de famille" className="input" required />
-            <input type="text" name="firstName" placeholder="Prénom" className="input" required />
-            <input type="text" name="idNumber" placeholder="Numéro carte d’identité" className="input" required />
-            <input type="email" name="email" placeholder="E-mail" className="input" required />
-            <input type="number" name="age" placeholder="Age" className="input" required />
-            <input type="password" name="password" placeholder="Mot de passe" className="input" required />
-            <input type="password" name="confirmPassword" placeholder="Confirmer mot de passe" className="input" required />
-            
-            <div className="idPhotoSection">
-              <label>Une photo de votre pièce d'identité incluant suffisamment votre nom, âge et photo</label>
-              <input type="file" accept="image/*" onChange={handleIdPhotoUpload} className="hidden" id="id-upload" />
-              <label htmlFor="id-upload" className="uploadBtn">Ajouter une photo</label>
-              {idPhotoName && <span className="file-name">{idPhotoName}</span>} {/* Show uploaded file name */}
-            </div>
+          <div className={styles.photoActions}>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageUpload}
+              className={styles.hidden}
+              id="file-upload"
+            />
+            <label htmlFor="file-upload" className={styles.uploadBtn}>
+              Télécharger une photo
+            </label>
+            <button className={styles.deleteBtn} onClick={handleDeletePhoto}>
+              Supprimer la photo
+            </button>
+          </div>
+        </div>
+        <div className={styles.registerCard}>
+          <h2 className={styles.registerTitle}>Inscrivez-vous</h2>
+          <div className={styles.contentWrapper}>
+            <form className={styles.registerForm} onSubmit={handleSubmit}>
+              <input
+                type="text"
+                name="lastName"
+                placeholder="Nom de famille"
+                className={styles.input}
+                required
+              />
+              <input
+                type="text"
+                name="firstName"
+                placeholder="Prénom"
+                className={styles.input}
+                required
+              />
+              <input
+                type="text"
+                name="idNumber"
+                placeholder="Numéro carte d’identité"
+                className={styles.input}
+                required
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="E-mail"
+                className={styles.input}
+                required
+              />
+              <input
+                type="number"
+                name="age"
+                placeholder="Age"
+                className={styles.input}
+                required
+              />
+              <input
+                type="password"
+                name="password"
+                placeholder="Mot de passe"
+                className={styles.input}
+                required
+              />
+              <input
+                type="password"
+                name="confirmPassword"
+                placeholder="Confirmer mot de passe"
+                className={styles.input}
+                required
+              />
 
-            <button type="submit" className="btn">S'inscrire</button>
-          </form>
+              <div className={styles.idPhotoSection}>
+                <label>
+                  Une photo de votre pièce d'identité incluant suffisamment
+                  votre nom, âge et photo
+                </label>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleIdPhotoUpload}
+                  className={styles.hidden}
+                  id="id-upload"
+                />
+                <label htmlFor="id-upload" className={styles.uploadBtn}>
+                  Ajouter une photo
+                </label>
+                {idPhotoName && (
+                  <span className={styles.fileName}>{idPhotoName}</span>
+                )}
+              </div>
+              <div className={styles.contactContainer}>
+                <label className={styles.contactLabel}>
+                  Ajouter un moyen de contact
+                </label>
+
+                <div className={styles.iconContainer}>
+                  <div
+                    className={styles.iconWrapper}
+                    onClick={() => handleIconClick("Facebook")}
+                  >
+                    <FaFacebook className={styles.socialicon} />
+                  </div>
+                  <div
+                    className={styles.iconWrapper}
+                    onClick={() => handleIconClick("Instagram")}
+                  >
+                    <FaInstagram className={styles.socialicon} />
+                  </div>
+                  <div
+                    className={styles.iconWrapper}
+                    onClick={() => handleIconClick("WhatsApp")}
+                  >
+                    <FaWhatsapp className={styles.socialicon} />
+                  </div>
+                </div>
+
+                {showModal && (
+                  <div className={styles.modal}>
+                    <div className={styles.modalContent}>
+                      <span
+                        className={styles.closeButton}
+                        onClick={handleCloseModal}
+                      >
+                        &times;
+                      </span>
+                      <h3>Connectez votre {selectedPlatform} account</h3>
+                      <p>
+                        Pour continuer, connectez-vous à votre compte{" "}
+                        {selectedPlatform}.
+                      </p>
+                      <button
+                        className={styles.button}
+                        style={{ margin: "10px" ,
+                          fontSize: "16px"
+                        }}
+                        onClick={handleCloseModal}
+                      >
+                        Connecter
+                      </button>
+                    </div>
+                  </div>
+                )}
+              </div>
+              <button type="submit" className={styles.button}>
+                S'inscrire
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
   );
 }
-
-export default RegisterPage;
