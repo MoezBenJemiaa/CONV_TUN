52b4d0f85cc52b16b92c65253de6e260b6762eb8
Rostom Turki <rostomtourki04@gmail.com>
2025-04-05 23:41:30 +0100

    Login page done

--- Front/src/Login/Login.jsx
+++ Front/src/Login/Login.jsx
@@ -1,38 +1,39 @@
 import { useState } from "react";
 import axios from "axios";
-import "./Login.css";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+import styles from "./Login.module.css";
+
 
 function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [rememberMe, setRememberMe] = useState(false);
-
-
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  try {
-    const response = await axios.post(
-      "http://localhost:5000/login",
-      { email, password },
-      { headers: { "Content-Type": "application/json" } }
-    );
-    
-    
-    localStorage.setItem("token", response.data.token);
-    window.location.href = "/"; // Redirect to home on success
-  } catch (error) {
-    alert("Login failed : "+ error.response?.data?.message || "Server error");
-  }
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/login",
+        { email, password },
+        { headers: { "Content-Type": "application/json" } }
+      );
+
+      localStorage.setItem("token", response.data.token);
+      window.location.href = "/"; // Redirect to home on success
+    } catch (error) {
+      alert(
+        "Login failed : " + error.response?.data?.message || "Server error"
+      );
+    }
   };
 
-  
   return (
-    <div className="login_container">
-      <div className="login-box">
-        <h2 className="title">Connecter-vous</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="input-group">
+    <div className={styles.loginContainer}>
+      <div className={styles.loginBox}>
+        <h2 className={styles.title}>Connecter-vous</h2>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <div className={styles.inputGroup}>
             <input
               type="email"
               placeholder="E-mail"
@@ -41,26 +42,36 @@ function LoginPage() {
               required
             />
           </div>
-          <div className="input-group">
+          <div className={styles.inputGroup}>
             <input
-              type="password"
+              type={showPassword ? "text" : "password"}
               placeholder="mot de passe"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
             />
+            {password.length > 0 && (
+              <span
+                onClick={() => setShowPassword(!showPassword)}
+                className={styles.passwordToggleIcon}
+              >
+                {showPassword ? <FiEyeOff /> : <FiEye />}
+              </span>
+            )}
           </div>
-          <div className="checkbox-group">
+          <div className={styles.checkboxGroup}>
             <input
               type="checkbox"
               checked={rememberMe}
               onChange={() => setRememberMe(!rememberMe)}
             />
-            <label>Se souvenir de moi</label>
+            Se souvenir de moi
           </div>
-          <div className="forgot-password">Mot de passe oublié ?</div>
-          <p class="signup-prompt">Pas encore un membre? <a href="/Register">Inscrivez-vous</a></p>
-          <button type="submit" className="login-button">
+          <div className={styles.forgotPassword}>Mot de passe oublié ?</div>
+          <p className={styles.signupPrompt}>
+            Pas encore un membre? <a href="/Register">Inscrivez-vous</a>
+          </p>
+          <button type="submit" className={styles.loginButton}>
             Connexion
           </button>
         </form>
