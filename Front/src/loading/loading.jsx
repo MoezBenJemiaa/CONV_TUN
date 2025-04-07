391aea45edb54cc09c022cddf1cb8605b796a531
Rostom Turki <rostomtourki04@gmail.com>
2025-04-07 18:18:02 +0100

    loading updated

--- Front/src/loading/loading.jsx
+++ Front/src/loading/loading.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import styles from './loading.module.css';
-import wheelImage from '../pictures/steering-wheel.png';
+import React from "react";
+import styles from "./loading.module.css";
+import wheelImage from "../pictures/steering-wheel.png";
+
 
 function Loading() {
   return (
-    <div className={styles.loadingContainer}>
-      <div className={styles.wheelTrack}>
-        <div className={styles.wheelPivot}>
-          <img 
-            src={wheelImage} 
-            className={styles.steeringWheel} 
-            alt="Loading"
-            draggable="false"
-          />
+    <div className={styles.loadingWrapper}>
+      <div className={styles.loadingContainer}>
+        <div className={styles.wheelTrack}>
+          <div className={styles.wheelPivot}>
+            <img
+              src={wheelImage}
+              className={styles.steeringWheel}
+              alt="Loading"
+              draggable="false"
+            />
+          </div>
         </div>
+        <p className={styles.loadingText}>Veuillez patientez . . .</p>
       </div>
-      <p className={styles.loadingText}>veuillez patientez . . .</p>
     </div>
   );
 }
